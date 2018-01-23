df685e4108c5e99a893e2d21212c10d4cb1e9fab
Bartłomiej Górny <bartlomiej.gorny@erlang-solutions.com>
2018-01-23 11:01:53 +0100

    work in progress on regression test for 'global' modules

--- test.disabled/ejabberd_tests/tests/conf_reload_SUITE.erl
+++ test.disabled/ejabberd_tests/tests/conf_reload_SUITE.erl
@@ -34,7 +34,8 @@
 %%--------------------------------------------------------------------
 
 all() ->
-    [{group, xmpp_domain_local_reload}].
+    [{group, xmpp_domain_local_reload},
+     {group, remove_add_host}].
 
 groups() ->
     [{xmpp_domain_local_reload, [],
@@ -43,7 +44,9 @@ groups() ->
        user_should_be_registered_and_unregistered_via_ctl,
        user_should_be_registered_and_unregistered_via_xmpp,
        user_should_be_disconnected_from_removed_domain
-       ]}].
+       ]},
+     {remove_add_host, [], [modules_do_not_disappear]}
+    ].
 
 suite() ->
     [{required, {hosts, mim, reloaded_domain}} | escalus:suite()].
@@ -66,10 +69,15 @@ init_per_group(xmpp_domain_local_reload, Config0) ->
     Config1 = create_user_in_initial_domain(?INITIAL_DOMAIN_USER,
                                             Config0),
     [{new_hosts_value, [?RELOADED_DOMAIN]},
-     {modify_config_file_fun, F} | Config1].
+     {modify_config_file_fun, F} | Config1];
+init_per_group(remove_add_host, Config) ->
+    F = fun(_) -> ok end,
+    [{modify_config_file_fun, F} | Config].
 
 end_per_group(xmpp_domain_local_reload, Config) ->
-    delete_user_in_initial_domain(?INITIAL_DOMAIN_USER, Config).
+    delete_user_in_initial_domain(?INITIAL_DOMAIN_USER, Config);
+end_per_group(_, Config) ->
+    Config.
 
 init_per_testcase(CaseName, Config0) ->
     Config1 = escalus:init_per_testcase(CaseName, Config0),
@@ -135,10 +143,61 @@ user_should_be_disconnected_from_removed_domain(Config) ->
     %% THEN
     ?assertNot(escalus_connection:is_connected(Conn)).
 
+modules_do_not_disappear(Config) ->
+    has_muc_command(), % is in default config
+    set_hosts_on_server([<<"host_a">>], Config),
+    has_muc_command(), % one added, three removed, should still be here
+    set_hosts_on_server([<<"host_a">>, <<"host_b">>], Config),
+    has_muc_command(), % etc.
+    set_hosts_on_server([<<"host_a">>], Config),
+    has_muc_command(),
+    set_hosts_on_server([], Config),
+    has_muc_command(false),
+    ok.
+
+module_refcount(Config) ->
+    % set host_config so that host_b and host_c have mod_admin_extra
+    set_hosts_on_server([<<"host_a">>], Config),
+    has_adm_command(false),
+    set_hosts_on_server([<<"host_a">>, <<"host_b">>], Config),
+    has_adm_command(),
+    set_hosts_on_server([<<"host_a">>, <<"host_b">>, <<"host_c">>], Config),
+    has_adm_command(),
+    set_hosts_on_server([<<"host_a">>, <<"host_b">>], Config),
+    has_adm_command(),
+    set_hosts_on_server([<<"host_a">>], Config),
+    has_adm_command(false),
+    ok.
 
 %%--------------------------------------------------------------------
 %% Internal functions
 %%--------------------------------------------------------------------
+
+has_muc_command() ->
+    Res = escalus_ejabberd:rpc(mongoose_commands, get_command, [admin, create_muc_room]),
+    ct:pal("Res: ~p", [Res]),
+%%    ?assertEqual(mongoose_command, element(1, Res)).
+    ok.
+
+has_muc_command(false) ->
+    Res = escalus_ejabberd:rpc(mongoose_commands, get_command, [admin, create_muc_room]),
+    ?assertEqual(error, element(1, Res)).
+
+has_adm_command() ->
+    Res = proplists:is_defined(kick_session,
+                               escalus_ejabberd:rpc(ejabberd_commands, list_commands, [])),
+    ct:pal("Res: ~p", [Res]),
+%%    ?assert(Res).
+    ok.
+
+has_adm_command(false) ->
+    Res = proplists:is_defined(kick_session,
+                               escalus_ejabberd:rpc(ejabberd_commands, list_commands, [])),
+    ?assertNot(Res).
+
+set_hosts_on_server(Hosts, Config) ->
+    set_hosts_in_server_config(Hosts, Config),
+    reload_through_ctl(default_node(Config), Config).
 
 get_ejabberd_hosts() ->
     escalus_ejabberd:rpc(ejabberd_config, get_global_option, [hosts]).
@@ -149,12 +208,25 @@ register_user_by_ejabberd_admin(User, Host) ->
 unregister_user_by_ejabberd_admin(User, Host) ->
     escalus_ejabberd:rpc(ejabberd_admin, unregister, [User, Host]).
 
+set_hosts_in_server_config(Hosts, Config) ->
+    ejabberd_node_utils:modify_config_file(
+        [mk_value_for_hosts_pattern(Hosts)], Config).
+
 change_domain_in_config_file(Config) ->
     ejabberd_node_utils:modify_config_file(
-      [mk_value_for_hosts_pattern(?RELOADED_DOMAIN)], Config).
+      [mk_value_for_hosts_pattern(?config(new_hosts_value, Config))], Config).
 
-mk_value_for_hosts_pattern(Domain) ->
-    {hosts, "[\"" ++ binary_to_list(Domain) ++ "\"]"}.
+mk_value_for_hosts_pattern(Domain) when is_binary(Domain) ->
+    {hosts, "[\"" ++ binary_to_list(Domain) ++ "\"]"};
+mk_value_for_hosts_pattern(Domains) when is_list(Domains) ->
+    {hosts,
+        "[" ++
+        string:join(
+                    lists:map(fun(H) -> "\"" ++ binary_to_list(H) ++ "\"" end,
+                              Domains),
+                    ",")
+        ++ "]"
+    }.
 
 run_config_file_modification_fun(Config) ->
     Fun = ?config(modify_config_file_fun, Config),
